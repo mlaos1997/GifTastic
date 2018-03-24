e0dd24d75e97a98228d982de58c7cfdba4859c99
Marcelo Laos <mlaos1997@gmail.com>
2018-03-24 00:36:26 -0400

    Images working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,8 @@ var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
     "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
     "capybara", "teacup pig", "serval", "salamander", "frog" ];
 
+// This function will create our animal buttons using our array
+//=====================================
 function animalButtons(animals) {
 	for (var i = 0; i < animals.length; i++) {
 		console.log(animals[i]);
@@ -30,18 +32,19 @@ function animalButtons(animals) {
 
 }
 animalButtons(animals);
-
 // Loading our GIFS onto our webpage
 //=================================================
 $('button').on('click', function() {
+// Resetting our values
+//=================================================
 	$('#animals').empty();
 	$('.header').empty();
+//Creating our selectors
+//=================================================
 	var x = $(this).data("type");
 	$('.header').append("<h1>" + x + "</h1>")
 
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q="+x+"&api_key=" + authKey + "&limit=10";
-
-
 
 //AJAX FUNCTION
 //==================================================
@@ -53,24 +56,25 @@ $('button').on('click', function() {
 // This for loop will print out 10 gifs to our page
 //==================================================
 	for (var i = 0; i < 10; i++) {
-// Creating states for our images (Ex: Still, Animated) *DELETED COME BACK TO TOMORROW :)* YOU GOT THIS!!
+// Creating variables for our still and animated images
 //=====================================================
 		var animated = response.data[i].images.fixed_height.url;
-		   var still = response.data[i].images.fixed_height_still.url;
+		var still = response.data[i].images.fixed_height_still.url;
 		var animalImage = $('<img>')
+// Adding attributes to our images
+//=====================================================
 		animalImage.attr("src", still);
 		animalImage.attr("data-still", still);
 		animalImage.attr("data-animate", animated);
 		animalImage.attr("data-state", "still");
 		animalImage.addClass("animal-image")
-
-
+// Appending our Rating and images
+//====================================================
 	    $('#animals').append("<p>Rating: " + response.data[i].rating + "</p>");
 		$('#animals').append(animalImage);
-
-
 	};
-		});
+// This function will pause and start our images
+//====================================================
 		$(document).on("click", ".animal-image" ,function () {
   			var state = $(this).attr("data-state");
   			if (state === "still") {
@@ -81,23 +85,24 @@ $('button').on('click', function() {
   				$(this).attr("data-state", "still");
   			}
 		})
+
+		});
+
   });
 
-// Here we will add img States, Still and animate
+
 
  // This function will add new animalBtns to our Array
  //================================================
 $('#animal-search').on('click', function(event) {
 	event.preventDefault();
-
-
  // Get value from user input and store it in a variable
  //=================================================
  var newAnimal = $('input').val().trim();
  animals.push(newAnimal);
 animalButtons(newAnimal).val().trim();
+animalButtons(animals);
 });
-
 
 });
 
